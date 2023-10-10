bcef9a7df70f64d4f01b46c3b2942faf9fab0806
Abdelmounim Baouia <abdelmounimbaouia@gmail.com>
2023-10-10 23:50:39 +0100

    feat: added a more optimised function functions execute on parallel

--- tp1/primalityTest.go
+++ tp1/primalityTest.go
@@ -36,7 +36,17 @@ func isPrime1(n int) bool {
   return nbDevisors(n) == 2
 }
 
+func isPrime2(n int) bool {
+  for i := 2; i < n / 2; i++ {
+    if n % i == 0 {
+      return false
+    }
+  }
+  return true
+}
+
 func main () {
+  go func() {
     fileName := "primalityTest1.csv"
 
     f, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
@@ -47,11 +57,33 @@ func main () {
 
     f.Truncate(0)
     f.WriteString("n, execTime\n")
-  for _, n := range numbers {
-    start := time.Now()
-    isPrime1(n)
-    execTime := time.Since(start)
-    fmt.Printf("isPrime1(%d) took %s\n", n, execTime)
-    f.WriteString(fmt.Sprintf("%d, %f\n", n, execTime.Seconds()))
-  }
+    for _, n := range numbers {
+      start := time.Now()
+      isPrime1(n)
+      execTime := time.Since(start)
+      fmt.Printf("isPrime1(%d) took %s\n", n, execTime)
+      f.WriteString(fmt.Sprintf("%d, %f\n", n, execTime.Seconds()))
+    }
+  }()
+  go func(){
+
+    fileName := "primalityTest2.csv"
+
+    f, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    f.Truncate(0)
+    f.WriteString("n, execTime\n")
+    for _, n := range numbers {
+      start := time.Now()
+      isPrime2(n)
+      execTime := time.Since(start)
+      fmt.Printf("isPrime2(%d) took %s\n", n, execTime)
+      f.WriteString(fmt.Sprintf("%d, %f\n", n, execTime.Seconds()))
+    }
+  }()
+  time.Sleep(10 * time.Second)
 }
